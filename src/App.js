393f65ada4f2b538bc9e0c934ce64162f1db2646
sejiniyam <osaejino123@gmail.com>
2024-07-04 14:48:21 +0900

    README 수정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. 안뇽! 리액트 공부 가보작오
         </p>
         <a
           className="App-link"
